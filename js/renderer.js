e96d507fe6f3998e8ec69486ddc5c64a67600c15
Alper Ş <alpersahistan@gmail.com>
2021-09-17 19:40:27 +0300

    Attempt to encapsulate webgpu api

--- js/renderer.js
+++ js/renderer.js
@@ -1,68 +1,65 @@
 "use strict";
-var curMesh = {};
-
-window.onload = function () {
-  document.getElementById("model-select").value = "car.obj";
-  loadSelection("car.obj");
-
-  render();
-};
-
-document.addEventListener("DOMContentLoaded", function () {
-  $.getJSON("./models", (options) => {
-    var elems = document.querySelectorAll("select");
-    var instances = M.FormSelect.init(elems, options);
-    let modS = document.getElementById("model-select");
-    for (let i of options) {
-      let opt = document.createElement("option");
-      opt.innerText = i;
-      modS.appendChild(opt);
-    }
-  });
-});
-
-function onModelSelect() {
-  //get selection
-  var x = document.getElementById("model-select").value;
-  loadSelection(x);
-}
-
-function loadSelection(value) {
-  //download
-  OBJ.downloadModels([
-    {
-      name: "cur",
-      obj: "models/" + value,
-      mtl: false,
-    },
-  ])
-    .then((data) => {
-      curMesh = data;
-      console.log("success: ", data);
-    })
-    .catch((e) => console.error("Failure:", e));
-}
-
-async function render() {
-  if (!navigator.gpu) {
-    document
-      .getElementById("webgpu-canvas")
-      .setAttribute("style", "display:none;");
-    document
-      .getElementById("no-webgpu")
-      .setAttribute("style", "display:block;");
-    return;
+var scene;
+var webAPI;
+class Scene {
+  constructor(canvas) {
+    this.curMesh = { a: 1, b: 2 };
+    this.model = mat4.create();
+    //this.view =
+    this.proj = mat4.perspective(
+      mat4.create(),
+      (50 * Math.PI) / 180.0,
+      canvas.width / canvas.height,
+      0.1,
+      100
+    );
+    this.projView = mat4.create();
+    this.camera = new ArcballCamera([0, 0, -20], [0, 0, 0], [0, 1, 0], 0.5, [
+      canvas.width,
+      canvas.height,
+    ]);
   }
+  loadSelection(value) {
+    //download
+    OBJ.downloadModels([
+      {
+        name: "cur",
+        obj: "models/" + value,
+        mtl: false,
+      },
+    ])
+      .then((data) => {
+        scene.curMesh = data;
+        console.log("success: ", scene);
+      })
+      .catch((e) => console.error("Failure:", e));
+  }
+}
 
-  // Get a GPU device to render with
-  var adapter = await navigator.gpu.requestAdapter();
-  var device = await adapter.requestDevice();
+class RenderAPI {
+  constructor() {
+    var stop = false;
 
-  // Get a context to display our rendered image on the canvas
-  var canvas = document.getElementById("webgpu-canvas");
-  var context = canvas.getContext("webgpu");
+    this.adapter = {}; //await navigator.gpu.requestAdapter();
+    this.device = {}; //await webAPI.adapter.requestDevice();
 
-  var shaderCode = `
+    this.depthFormat = "depth24plus-stencil8";
+    this.depthTexture = {}; /*this.device.createTexture({
+      size: { width: canvas.width, height: canvas.height, depth: 1 },
+      format: this.depthFormat,
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });*/
+    this.renderPassDesc = {
+      /*colorAttachments: [{ view: undefined, loadValue: [0.9, 0.3, 0.3, 1] }],
+      depthStencilAttachment: {
+        view: this.depthTexture.createView(),
+        depthLoadValue: 1.0,
+        depthStoreOp: "store",
+        stencilLoadValue: 0,
+        stencilStoreOp: "store",
+      },*/
+    };
+    this.shaderCode = `
   type float4 = vec4<f32>;
   type float3 = vec3<f32>;
   struct VertexInput {
@@ -95,11 +92,305 @@ async function render() {
   fn fragment_main(in: VertexOutput) -> [[location(0)]] float4 {
       //var light_dir = vec4<f32>(0.0,-1.0,0.0,1.0);
       return float4(in.color /* *(light_dir * in.position)*/);
+  }`;
+
+    this.shaderModule = {};
+    this.vertexState = {};
+    this.fragmentState = {};
+    this.bindGroupLayout = {};
+    this.renderPipeline = {};
+    this.viewParamsBuffer = {};
+    this.viewParamBG = {};
   }
-  `;
+  async initDevice() {
+    this.adapter = await navigator.gpu.requestAdapter();
+    this.device = await this.adapter.requestDevice();
+    //while (this.device == null) {}
+  }
+
+  async compileShaderModules(canvas) {
+    var context = canvas.getContext("webgpu");
+    this.shaderModule = this.device.createShaderModule({
+      code: this.shaderCode,
+    });
+    // This API is only available in Chrome right now
+    if (this.shaderModule.compilationInfo) {
+      var compilationInfo = await this.shaderModule.compilationInfo();
+      if (compilationInfo.messages.length > 0) {
+        var hadError = false;
+        console.log("Shader compilation log:");
+        for (var i = 0; i < compilationInfo.messages.length; ++i) {
+          var msg = compilationInfo.messages[i];
+          console.log(`${msg.lineNum}:${msg.linePos} - ${msg.message}`);
+          hadError = hadError || msg.type == "error";
+        }
+        if (hadError) {
+          console.log("Shader failed to compile");
+          return;
+        }
+      }
+    }
+
+    this.depthTexture = this.device.createTexture({
+      size: { width: canvas.width, height: canvas.height, depth: 1 },
+      format: this.depthFormat,
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+
+    this.renderPassDesc = {
+      colorAttachments: [{ view: undefined, loadValue: [0.9, 0.3, 0.3, 1] }],
+      depthStencilAttachment: {
+        view: this.depthTexture.createView(),
+        depthLoadValue: 1.0,
+        depthStoreOp: "store",
+        stencilLoadValue: 0,
+        stencilStoreOp: "store",
+      },
+    };
+
+    // Vertex attribute state and shader stage
+    this.vertexState = {
+      module: this.shaderModule,
+      entryPoint: "vertex_main",
+      buffers: [
+        {
+          arrayStride: 4 * 3,
+          attributes: [
+            { format: "float32x3", offset: 0, shaderLocation: 0 },
+            //{ format: "float32x4", offset: 4 * 4, shaderLocation: 1 },
+          ],
+        },
+      ],
+    };
+
+    // Setup render outputs
+    var swapChainFormat = "bgra8unorm";
+    context.configure({
+      device: this.device,
+      format: swapChainFormat,
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+
+    // Fragment output targets and shader stage
+    this.fragmentState = {
+      module: this.shaderModule,
+      entryPoint: "fragment_main",
+      targets: [{ format: swapChainFormat }],
+    };
+
+    // Create bind group layout
+    this.bindGroupLayout = console.log(this.device.createBindGroupLayout({
+      entries: [
+        {
+          binding: 0,
+          visibility: GPUShaderStage.VERTEX,
+          buffer: { type: "uniform" },
+        },
+      ],
+    }));
+
+    // Create render pipeline
+    var layout = this.device.createPipelineLayout({
+      bindGroupLayouts: [bindGroupLayout],
+    });
+
+    this.renderPipeline = this.device.createRenderPipeline({
+      layout: layout,
+      vertex: vertexState,
+      fragment: fragmentState,
+      depthStencil: {
+        format: this.depthFormat,
+        depthWriteEnabled: true,
+        depthCompare: "less",
+      },
+    });
+
+    // Create a buffer to store the view parameters
+    this.viewParamsBuffer = this.device.createBuffer({
+      size: 16 * 4,
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+
+    this.viewParamBG = this.device.createBindGroup({
+      layout: bindGroupLayout,
+      entries: [{ binding: 0, resource: { buffer: viewParamsBuffer } }],
+    });
+  }
+
+  initBuffers(mesh) {
+    var vertexBuffer = webAPI.device.createBuffer({
+      size: mesh.vertices.length * 4,
+      usage: GPUBufferUsage.VERTEX,
+      mappedAtCreation: true,
+    });
+    //positions
+    new Float32Array(vertexBuffer.getMappedRange()).set(mesh.vertices);
+    vertexBuffer.unmap();
+
+    // Specify index data
+    var indexBuffer = webAPI.device.createBuffer({
+      size: mesh.indices.length * 4,
+      usage: GPUBufferUsage.INDEX,
+      mappedAtCreation: true,
+    });
+    //indices
+    new Uint32Array(indexBuffer.getMappedRange()).set(mesh.indices),
+      indexBuffer.unmap();
+  }
+}
+
+async function init() {
+  await webAPI.initDevice();
+  await webAPI.compileShaderModules(document.getElementById("webgpu-canvas"));
+  webAPI.initBuffers(scene.curMesh.cur);
+}
+
+window.onload = async function () {
+  scene = new Scene(document.getElementById("webgpu-canvas"));
+  webAPI = new RenderAPI();
+  document.getElementById("model-select").value = "box.obj";
+  await scene.loadSelection("box.obj");
+  await init();
+  main();
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  $.getJSON("./models", (options) => {
+    var elems = document.querySelectorAll("select");
+    var instances = M.FormSelect.init(elems, options);
+    let modS = document.getElementById("model-select");
+    for (let i of options) {
+      let opt = document.createElement("option");
+      opt.innerText = i;
+      modS.appendChild(opt);
+    }
+  });
+});
+
+function onModelSelect() {
+  //get selection
+  var x = document.getElementById("model-select").value;
+  loadSelection(x);
+}
+
+/*function loadSelection(value) {
+  //download
+  OBJ.downloadModels([
+    {
+      name: "cur",
+      obj: "models/" + value,
+      mtl: false,
+    },
+  ])
+    .then((data) => {
+      scene.curMesh = data;
+      console.log("success: ", scene);
+    })
+    .catch((e) => console.error("Failure:", e));
+}*/
+
+function drawFrame() {
+  if (!webAPI.stop) {
+    // Update camera buffer
+    scene.projView = mat4.mul(scene.projView, scene.proj, scene.camera);
+
+    var upload = webAPI.device.createBuffer({
+      size: 16 * 4,
+      usage: GPUBufferUsage.COPY_SRC,
+      mappedAtCreation: true,
+    });
+    {
+      var map = new Float32Array(upload.getMappedRange());
+      map.set(scene.projView);
+      upload.unmap();
+    }
+
+    var canvas = document.getElementById("webgpu-canvas");
+    var context = canvas.getContext("webgpu");
+    webAPI.renderPassDesc.colorAttachments[0].view = context
+      .getCurrentTexture()
+      .createView();
+
+    var commandEncoder = webAPI.device.createCommandEncoder();
+    commandEncoder.copyBufferToBuffer(
+      upload,
+      0,
+      webAPI.viewParamsBuffer,
+      0,
+      16 * 4
+    );
+
+    var renderPass = commandEncoder.beginRenderPass(webAPI.renderPassDesc);
+
+    renderPass.setPipeline(renderPipeline);
+    renderPass.setBindGroup(0, viewParamBG);
+    renderPass.setVertexBuffer(0, vertexBuffer);
+    renderPass.setIndexBuffer(indexBuffer, "uint32");
+    renderPass.drawIndexed(scene.curMesh.cur.indices.length);
+
+    renderPass.endPass();
+    webAPI.device.queue.submit([commandEncoder.finish()]);
+  }
+  requestAnimationFrame(drawFrame);
+}
+
+async function main() {
+  if (!navigator.gpu) {
+    document
+      .getElementById("webgpu-canvas")
+      .setAttribute("style", "display:none;");
+    document
+      .getElementById("no-webgpu")
+      .setAttribute("style", "display:block;");
+    return;
+  }
+
+  // Get a GPU device to render with
+  /*webAPI.adapter = await navigator.gpu.requestAdapter();
+  webAPI.device = await webAPI.adapter.requestDevice();*/
+
+  // Get a context to display our rendered image on the canvas
+  var canvas = document.getElementById("webgpu-canvas");
+  var context = canvas.getContext("webgpu");
+
+  /*var shaderCode = `
+  type float4 = vec4<f32>;
+  type float3 = vec3<f32>;
+  struct VertexInput {
+      [[location(0)]] position: float3;
+      //[[location(1)]] color: float4;
+  };
+
+  struct VertexOutput {
+      [[builtin(position)]] position: float4;
+      [[location(0)]] color: float4;
+  };
+
+  [[block]]
+  struct ViewParams {
+      view_proj: mat4x4<f32>;
+  };
+
+  [[group(0), binding(0)]]
+  var<uniform> view_params: ViewParams;
+
+  [[stage(vertex)]]
+  fn vertex_main(vert: VertexInput) -> VertexOutput {
+      var out: VertexOutput;
+      out.color =  vec4<f32>(0.0, 0.0, 1.0, 1.0);
+      out.position = view_params.view_proj * vec4<f32>(vert.position, 1.0);
+      return out;
+  };
+
+  [[stage(fragment)]]
+  fn fragment_main(in: VertexOutput) -> [[location(0)]] float4 {
+      //var light_dir = vec4<f32>(0.0,-1.0,0.0,1.0);
+      return float4(in.color  *(light_dir * in.position));
+  }
+  `;*/
 
   // Setup shader modules
-  var shaderModule = device.createShaderModule({ code: shaderCode });
+  /*var shaderModule = webAPI.device.createShaderModule({ code: shaderCode });
   // This API is only available in Chrome right now
   if (shaderModule.compilationInfo) {
     var compilationInfo = await shaderModule.compilationInfo();
@@ -116,31 +407,32 @@ async function render() {
         return;
       }
     }
-  }
-
+  }*/
   // Specify vertex data
-  var vertexBuffer = device.createBuffer({
-    size: curMesh.cur.vertices.length * 4,
+  /*var vertexBuffer = webAPI.device.createBuffer({
+    size: scene.curMesh.cur.vertices.length * 4,
     usage: GPUBufferUsage.VERTEX,
     mappedAtCreation: true,
   });
   //positions
-  new Float32Array(vertexBuffer.getMappedRange()).set(curMesh.cur.vertices);
+  new Float32Array(vertexBuffer.getMappedRange()).set(
+    scene.curMesh.cur.vertices
+  );
   vertexBuffer.unmap();
 
   // Specify index data
-  var indexBuffer = device.createBuffer({
-    size: curMesh.cur.indices.length * 4,
+  var indexBuffer = webAPI.device.createBuffer({
+    size: scene.curMesh.cur.indices.length * 4,
     usage: GPUBufferUsage.INDEX,
     mappedAtCreation: true,
   });
   //indices
-  new Uint32Array(indexBuffer.getMappedRange()).set(curMesh.cur.indices),
-    indexBuffer.unmap();
+  new Uint32Array(indexBuffer.getMappedRange()).set(scene.curMesh.cur.indices),
+    indexBuffer.unmap();*/
 
   // Vertex attribute state and shader stage
-  var vertexState = {
-    module: shaderModule,
+  /*var vertexState = {
+    module: webAPI.shaderModule,
     entryPoint: "vertex_main",
     buffers: [
       {
@@ -156,27 +448,27 @@ async function render() {
   // Setup render outputs
   var swapChainFormat = "bgra8unorm";
   context.configure({
-    device: device,
+    device: webAPI.device,
     format: swapChainFormat,
     usage: GPUTextureUsage.RENDER_ATTACHMENT,
-  });
+  });*/
 
-  var depthFormat = "depth24plus-stencil8";
-  var depthTexture = device.createTexture({
+  /*var depthFormat = "depth24plus-stencil8";
+  var depthTexture = webAPI.device.createTexture({
     size: { width: canvas.width, height: canvas.height, depth: 1 },
     format: depthFormat,
     usage: GPUTextureUsage.RENDER_ATTACHMENT,
-  });
+  });*/
 
   // Fragment output targets and shader stage
-  var fragmentState = {
-    module: shaderModule,
+  /*var fragmentState = {
+    module: webAPI.shaderModule,
     entryPoint: "fragment_main",
     targets: [{ format: swapChainFormat }],
   };
 
   // Create bind group layout
-  var bindGroupLayout = device.createBindGroupLayout({
+  var bindGroupLayout = webAPI.device.createBindGroupLayout({
     entries: [
       {
         binding: 0,
@@ -187,55 +479,44 @@ async function render() {
   });
 
   // Create render pipeline
-  var layout = device.createPipelineLayout({
+  var layout = webAPI.device.createPipelineLayout({
     bindGroupLayouts: [bindGroupLayout],
   });
 
-  var renderPipeline = device.createRenderPipeline({
+  var renderPipeline = webAPI.device.createRenderPipeline({
     layout: layout,
     vertex: vertexState,
     fragment: fragmentState,
     depthStencil: {
-      format: depthFormat,
+      format: webAPI.depthFormat,
       depthWriteEnabled: true,
       depthCompare: "less",
     },
   });
 
-  var renderPassDesc = {
-    colorAttachments: [{ view: undefined, loadValue: [0.3, 0.3, 0.3, 1] }],
-    depthStencilAttachment: {
-      view: depthTexture.createView(),
-      depthLoadValue: 1.0,
-      depthStoreOp: "store",
-      stencilLoadValue: 0,
-      stencilStoreOp: "store",
-    },
-  };
-
   // Create a buffer to store the view parameters
-  var viewParamsBuffer = device.createBuffer({
+  var viewParamsBuffer = webAPI.device.createBuffer({
     size: 16 * 4,
     usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
   });
 
-  var viewParamBG = device.createBindGroup({
+  var viewParamBG = webAPI.device.createBindGroup({
     layout: bindGroupLayout,
     entries: [{ binding: 0, resource: { buffer: viewParamsBuffer } }],
-  });
+  });*/
 
-  var camera = new ArcballCamera([0, 0, -20], [0, 0, 0], [0, 1, 0], 0.5, [
+  /*var camera = new ArcballCamera([0, 0, -20], [0, 0, 0], [0, 1, 0], 0.5, [
     canvas.width,
     canvas.height,
   ]);
-  var proj = mat4.perspective(
+  scene.proj = mat4.perspective(
     mat4.create(),
     (50 * Math.PI) / 180.0,
     canvas.width / canvas.height,
     0.1,
     100
   );
-  var projView = mat4.create();
+  scene.projView = mat4.create();*/
 
   // Register mouse and touch listeners
   var controller = new Controller();
@@ -257,54 +538,17 @@ async function render() {
 
   // Not covered in the tutorial: track when the canvas is visible
   // on screen, and only render when it is visible.
-  var canvasVisible = false;
   var observer = new IntersectionObserver(
     function (e) {
       if (e[0].isIntersecting) {
-        canvasVisible = true;
+        webAPI.stop = false;
       } else {
-        canvasVisible = false;
+        webAPI.stop = true;
       }
     },
     { threshold: [0] }
   );
   observer.observe(canvas);
 
-  var frame = function () {
-    if (canvasVisible) {
-      // Update camera buffer
-      projView = mat4.mul(projView, proj, camera.camera);
-
-      var upload = device.createBuffer({
-        size: 16 * 4,
-        usage: GPUBufferUsage.COPY_SRC,
-        mappedAtCreation: true,
-      });
-      {
-        var map = new Float32Array(upload.getMappedRange());
-        map.set(projView);
-        upload.unmap();
-      }
-
-      renderPassDesc.colorAttachments[0].view = context
-        .getCurrentTexture()
-        .createView();
-
-      var commandEncoder = device.createCommandEncoder();
-      commandEncoder.copyBufferToBuffer(upload, 0, viewParamsBuffer, 0, 16 * 4);
-
-      var renderPass = commandEncoder.beginRenderPass(renderPassDesc);
-
-      renderPass.setPipeline(renderPipeline);
-      renderPass.setBindGroup(0, viewParamBG);
-      renderPass.setVertexBuffer(0, vertexBuffer);
-      renderPass.setIndexBuffer(indexBuffer, "uint32");
-      renderPass.drawIndexed(curMesh.cur.indices.length);
-
-      renderPass.endPass();
-      device.queue.submit([commandEncoder.finish()]);
-    }
-    requestAnimationFrame(frame);
-  };
-  requestAnimationFrame(frame);
+  requestAnimationFrame(drawFrame);
 }
